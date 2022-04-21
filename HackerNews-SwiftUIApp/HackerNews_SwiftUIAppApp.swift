08a834c4e67a459746f745fae66e0474b0ac5b12
Santiago Rodriguez Affonso <rodriguezsant@icloud.com>
2022-04-21 16:44:59 -0300

    Initial Commit

---
+++ HackerNews-SwiftUIApp/HackerNews_SwiftUIAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  HackerNews_SwiftUIAppApp.swift
+//  HackerNews-SwiftUIApp
+//
+//  Created by Santiago Rodriguez Affonso on 21/04/2022.
+//
+
+import SwiftUI
+
+@main
+struct HackerNews_SwiftUIAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
