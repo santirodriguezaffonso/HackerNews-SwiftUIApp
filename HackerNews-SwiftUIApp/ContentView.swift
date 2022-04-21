95beb02a03ad3ceba2a324ddc35f12dc73342d97
Santiago Rodriguez Affonso <rodriguezsant@icloud.com>
2022-04-21 17:32:34 -0300

    Struct created. List component updated to use a reusable cell.

--- HackerNews-SwiftUIApp/ContentView.swift
+++ HackerNews-SwiftUIApp/ContentView.swift
@@ -8,11 +8,17 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    let posts: [Post] = [
+        Post(id: "1", title: "Bla, Bla, Bla"),
+        Post(id: "4", title: "New York City"),
+        Post(id: "4", title: "Minnesota")
+    ]
     var body: some View {
         NavigationView {
-            List {
-                Text("Hello!")
-            }
+            List(posts, rowContent: { post in
+                Text(post.title)
+            })
             .navigationTitle("Hacker News")
         }
     }
@@ -22,4 +28,10 @@ struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
+}
+
+
+struct Post: Identifiable {
+    let id: String
+    let title: String
 }
