895d88c53014d263cafb491d7278270f6088acc2
Santiago Rodriguez Affonso <rodriguezsant@icloud.com>
2022-04-22 19:20:44 -0300

    WebView added. App finished

---
+++ HackerNews-SwiftUIApp/Views/WebView.swift
@@ -0,0 +1,29 @@
+//
+//  WebView.swift
+//  HackerNews-SwiftUIApp
+//
+//  Created by Santiago Rodriguez Affonso on 22/04/2022.
+//
+
+import Foundation
+import SwiftUI
+import WebKit
+
+struct WebView: UIViewRepresentable {
+    typealias UIViewType = WKWebView
+    
+    let urlString: String?
+    
+    func makeUIView(context: Context) -> WKWebView {
+        return WKWebView()
+    }
+    
+    func updateUIView(_ uiView: WKWebView, context: Context) {
+        if let safeString = urlString {
+            if let url = URL(string: safeString) {
+                let request = URLRequest(url: url)
+                uiView.load(request)
+            }
+        }
+    }
+}
